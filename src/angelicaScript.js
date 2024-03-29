bc0fd1c849cd1a17ca4eb9927e7fe3c8a79551ab
angycapuozzo <angelicacapuozzo01@gmail.com>
2024-03-29 18:36:16 +0100

    angelica capuozzo

---
+++ src/angelicaScript.js
@@ -0,0 +1,18 @@
+function Accensione () {
+let accensione = document.getElementById("Accendi");
+let pos = 0;
+
+accensione.style.left = pos + 'px';
+}
+
+function Spento(){
+let accensione = document.getElementById("Accendi");
+let pos = 450;
+
+accensione.style.left = pos + 'px';
+}
+
+
+
+
+
