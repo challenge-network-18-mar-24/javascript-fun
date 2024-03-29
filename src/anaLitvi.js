2397762f91e44980040bab52e3af00a7cc80df2e
analitvi <154239690+AnaLitvi@users.noreply.github.com>
2024-03-29 20:57:22 +0100

    Duck_Game

---
+++ src/anaLitvi.js
@@ -0,0 +1,36 @@
+function goToStart() {
+    let myDuck = document.getElementById("myDuck");   
+    let pos = 0;
+  
+    myDuck.style.left = pos + '100px';
+}
+
+function goToEnd() {
+let myDuck = document.getElementById("myDuck");   
+let pos = 0;
+
+myDuck.style.left ='650px'; 
+}
+
+function goToUp() {
+let myDuck = document.getElementById("myDuck");   
+let pos = 0;
+
+myDuck.style.top = '40px'; 
+}
+
+function goToDown() {
+    let myDuck = document.getElementById("myDuck");   
+    let pos = 0;
+
+    myDuck.style.top = '400px'; 
+}
+
+function goToHome() {
+    let myDuck = document.getElementById("myDuck");   
+    let pos = 0;
+
+    myDuck.style.left= '-80px';
+    myDuck.style.top='-10px';
+
+}
