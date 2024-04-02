6e8411216259e106c7638a994cde4cb03ad589e6
andreamagazzini <andreamaga4@gmail.com>
2024-04-02 11:16:55 +0200

    add check age function

--- src/andreaScript.js
+++ src/andreaScript.js
@@ -1,13 +1,14 @@
-function goToStart() {
-  let myBlock = document.getElementById("myBlock");   
-  let pos = 0;
+function verificaEta() {
+  let mioInput = document.getElementById("mioInput");
+  let eta = mioInput.value;
 
-  myBlock.style.left = pos + 'px';
-}
-
-function goToEnd() {
-  let myBlock = document.getElementById("myBlock");   
-  let pos = 350;
-
-  myBlock.style.left = pos + 'px'; 
+  if (eta < 3) {
+    alert("L'ingresso e' gratuito");
+  } else if (eta >= 3 && eta < 12) {
+    alert("L'ingresso e' 5 euro");
+  } else if (eta >= 12 && eta < 65) {
+    alert("L'ingresso e' 10 euro");
+  } else {
+    alert("L'ingresso e' 7 euro");
+  }
 }
