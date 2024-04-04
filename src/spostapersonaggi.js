b44826d6615567014b966121e02b27cb969004a6
SofiaAbou <sofia.abouabdillah1@gmail.com>
2024-04-04 16:14:11 +0200

    test

---
+++ src/spostapersonaggi.js
@@ -0,0 +1,64 @@
+let opzioniDx = ["Paperino", "Topolino", "Pippo"];
+let opzioniSx = ["Pluto", "Clarabella", "Eta Beta"];
+
+let selectDx = document.getElementById("selectDx");
+let selectSx = document.getElementById("selectSx");
+
+function generaSelect() {
+    selectDx.innerHTML = "<option value=''></option>";
+    selectSx.innerHTML = "<option value=''></option>";
+
+    // Creazione select a destra
+    for (let x = 0; x < opzioniDx.length; x++) {
+        let nomeOpzione = opzioniDx[x];
+
+        // Crea opzione
+        let option = document.createElement("option");
+        option.innerHTML = nomeOpzione;
+        option.value = x;
+
+        // Aggungi opzione alla select
+        selectDx.appendChild(option);
+    }
+
+    // Creazione select a sinistra
+    let x = 0;
+    while (x < opzioniSx.length) {
+        let nomeOpzione = opzioniSx[x];
+
+        // Crea opzione
+        let option = new Option(nomeOpzione, x);
+
+        // Aggungi opzione alla select
+        selectSx.options.add(option);
+        x++
+    }
+}
+
+generaSelect();
+
+function spostaDx() {
+    let indice = selectSx.value;
+    let nomeOpzione = opzioniSx[indice]
+
+    // aggiungi all'array lato dx
+    opzioniDx.push(nomeOpzione);
+
+    // togli dall'array lato sx
+    opzioniSx.splice(indice, 1);
+
+    generaSelect();
+}
+
+function spostaSx() {
+    let indice = selectDx.value;
+    let nomeOpzione = opzioniDx[indice]
+
+    // aggiungi all'array lato dx
+    opzioniDx.push(nomeOpzione);
+
+    // togli dall'array lato sx
+    opzioniDx.splice(indice, 1);
+
+    generaSelect();
+}
