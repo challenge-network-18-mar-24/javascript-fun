e219555cca5727066b563068037be9c99e7e3b98
angycapuozzo <angelicacapuozzo01@gmail.com>
2024-04-04 17:38:01 +0200

    sposta blocco

--- src/angelicaScript.js
+++ src/angelicaScript.js
@@ -1,12 +1,12 @@
 function Accensione () {
-let accensione = document.getElementById("Accendi");
+let accensione = document.getElementById("blocco");
 let pos = 0;
 
 accensione.style.left = pos + 'px';
 }
 
 function Spento(){
-let accensione = document.getElementById("Accendi");
+let accensione = document.getElementById("blocco");
 let pos = 450;
 
 accensione.style.left = pos + 'px';
