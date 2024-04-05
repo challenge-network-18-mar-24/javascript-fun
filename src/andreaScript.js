5b15c9c21a0c8c82e8f00722143b20748b68d0be
Christian95Sp <spinachristian95@gmail.com>
2024-04-05 17:29:01 +0200

    js

--- src/andreaScript.js
+++ src/andreaScript.js
@@ -1,4 +1,4 @@
-let categorie = ["diney", "pokemon", "dragonball"];
+let categorie = ["disney", "pokemon", "dragonball"];
 
 let elementi = {
   0: ["pippo", "topolino"],
@@ -6,31 +6,50 @@ let elementi = {
   2: ["vegeta", "goku"]
 }
 
+// Funzione che crea una card categoria
+let creaCategoria = (nomeCategoria, indice) => {
+  let divCategoria = document.createElement("div");
+  divCategoria.className = "card";
+  divCategoria.id = "categoria" + indice
+
+  // Titolo div categoria
+  let spanTitolo = document.createElement("span");
+  spanTitolo.className = "titolo"
+  spanTitolo.innerHTML = nomeCategoria;
+
+  // input elementi categoria 
+  let input = document.createElement("input");
+  input.id = "input" + indice;
+
+  divCategoria.appendChild(spanTitolo);
+  divCategoria.appendChild(input);
+
+  // Append elementi al divCategoria
+  elementi[indice].forEach((nomeElemento) => {
+    let spanElemento = document.createElement("span");
+    spanElemento.innerHTML = nomeElemento;
+
+    divCategoria.appendChild(spanElemento)
+  })
+
+  return divCategoria;
+}
+
+// Funzione per fare il render delle categorie
 let renderCategorie = () => {
   let container = document.getElementById("container");
 
   container.innerHTML = "";
 
   categorie.forEach((nomeCategoria, indice) => {
-    let divCategoria = document.createElement("div");
-    
-    // Titolo div categoria
-    let spanTitolo = document.createElement("span");
-    spanTitolo.innerHTML = nomeCategoria;
-    
-    // input elementi categoria 
-    let input = document.createElement("input");
-    input.id = "input" + indice;
-
-    divCategoria.appendChild(spanTitolo);
-    divCategoria.appendChild(input);
-
+    let divCategoria = creaCategoria(nomeCategoria, indice);
     container.appendChild(divCategoria);
   })
 }
 
 renderCategorie();
 
+// Bottone per aggiungere un'altra categoria
 let aggiungiCategoria = () => {
   let inputCategoria = document.getElementById("inputCategoria");
   let nomeCategoria = inputCategoria.value;
